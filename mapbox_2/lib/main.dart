eaf7999b0068d01f35d8f7817ef0cb01df19f81f
kroye0213 <80084867+kroye0213@users.noreply.github.com>
2024-01-24 11:42:05 -0600

    Map Update
    YAY MAP UPDATE

--- mapbox_2/lib/main.dart
+++ mapbox_2/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart' as LatLng;
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,36 +29,57 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  LatLng.LatLng? _currentLocation;
+  MapController mapController = MapController();
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocation();
+  }
+
+  Future<void> _getLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition();
+      _currentLocation = LatLng.LatLng(position.latitude, position.longitude);
+      mapController.move(_currentLocation!, 14.0); // Adjust zoom level
+      setState(() {});
+    } catch (e) {
+      print("Error getting location: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    LatLng.LatLng initialLocation = _currentLocation ?? LatLng.LatLng(34.6275, -84.1935);
+
     return Scaffold(
       body: Stack(
         children: [
           FlutterMap(
+            mapController: mapController,
             options: MapOptions(
-              center: LatLng.LatLng(41.922800, -88.075600), // Starting point of the Appalachian Trail
-              zoom: 16,
+              center: initialLocation,
+              zoom: 14.0, // Adjust initial zoom level
             ),
             children: [
               TileLayer(
                 urlTemplate: 'https://api.mapbox.com/styles/v1/kroye01/clrp2zg4o005z01pfcbr00eup/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia3JveWUwMSIsImEiOiJjbHJwM2lsZjAwMW1rMnFvZHk5Y2lucHRrIn0.mDHtkPy7Jpz6wkNBrEzfgw',
                 additionalOptions: {
-                  'accessToken' : 'pk.eyJ1Ijoia3JveWUwMSIsImEiOiJjbHJwM2lsZjAwMW1rMnFvZHk5Y2lucHRrIn0.mDHtkPy7Jpz6wkNBrEzfgw',
-                  'id' : 'mapbox.mapbox-terrain-v2'
+                  'accessToken': 'pk.eyJ1Ijoia3JveWUwMSIsImEiOiJjbHJwM2lsZjAwMW1rMnFvZHk5Y2lucHRrIn0.mDHtkPy7Jpz6wkNBrEzfgw',
+                  'id': 'mapbox.mapbox-terrain-v2'
                 },
               ),
             ],
-            
           ),
           Positioned(
             bottom: 16,
             right: 16,
             child: FloatingActionButton(
               onPressed: () {
-                // Add your button click logic here
-                print("Hello button clicked!");
+                _getLocation(); // Update location on button press
               },
-              child: Text("Hello"),
+              child: Icon(Icons.location_searching),
             ),
           ),
         ],
