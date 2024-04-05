98947569b855bd6b4c1d6873acfa47c6e25d1620
alexisrua <alexisr02@icloud.com>
2024-04-04 22:12:52 -0500

    store user data, update login/signup page

--- mapbox_2/lib/auth.dart
+++ mapbox_2/lib/auth.dart
@@ -1,6 +1,42 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'signUp.dart';
+import 'login.dart';
 
-class Auth{
+
+class Auth extends StatefulWidget {
+  const Auth({Key? key}) : super(key: key);
+
+  @override
+  State<Auth> createState() => _AuthState();
+}
+
+  class _AuthState extends State<Auth> {
+    bool _isLogin = true; // Changed to true for default login screen
+
+    void toggleScreens(){
+      setState(() {
+        _isLogin = !_isLogin;
+      });
+
+    }
+    bool _loading = false;
+    String _errorMessage = ''; // Track error message
+
+  @override
+    Widget build(BuildContext context) {
+    if (_isLogin) {
+      return LoginPage(showSignUpPage: toggleScreens);
+    }else{
+      return signUpPage(showLoginPage: toggleScreens);
+    }
+
+  }
+
+
+
+
+
   final FirebaseAuth auth = FirebaseAuth.instance;
 
   Future<void> registerWithEmailandPassword(String email, String password) async {
