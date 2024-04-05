ff19129b2089478aa1582c761592cf5e9596001f
alexisrua <alexisr02@icloud.com>
2024-04-04 22:51:37 -0500

    took out login/sign up code from main

--- mapbox_2/lib/auth.dart
+++ mapbox_2/lib/auth.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'signUp.dart';
-import 'login.dart';
+import '/pages/signUp.dart';
+import '/pages/login.dart';
 
 
 class Auth extends StatefulWidget {
